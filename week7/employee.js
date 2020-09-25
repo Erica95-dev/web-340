7dbbbaa66209c7fff4e8844501f969bddb5edad4
Ericadp.developer <ericadp.developer@gmail.com>
2020-09-24 22:40:19 -0400

    Add files via upload

---
+++ week7/employee.js
@@ -0,0 +1,12 @@
+// Require statements
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Employee Schema
+let EmployeeSchema = new Schema({
+  firstName: {type: String, required: true },
+  lastName: {type: String, required: true }
+});
+
+// Export the model so its publicly available.
+module.exports = mongoose.model('Employee', EmployeeSchema);
